96726bfbde0cb997329a49b291346fdf7db41e97
chichenghongye <1639700823@qq.com>
2018-05-04 09:54:32 +0800

    v1-新增kafka的简单示例

---
+++ src/main/java/com/example/demo/kafka/KafkaProducer.java
@@ -0,0 +1,22 @@
+package com.example.demo.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableScheduling
+public class KafkaProducer {
+    @Value("${spring.kafka.topic}")
+    private String topic;
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
+
+    @Scheduled(fixedRate = 3000)
+    public void send() {
+        kafkaTemplate.send(topic, "哈喽"+System.currentTimeMillis());
+    }
+}
