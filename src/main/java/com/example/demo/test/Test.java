21137e943126efae8a982644c7cf4613f20b4f70
chichengfengye <1639700823@qq.com>
2018-07-22 13:33:34 +0800

    没有更新

---
+++ src/main/java/com/example/demo/test/Test.java
@@ -0,0 +1,8 @@
+package com.example.demo.test;
+
+public class Test {
+    public static void main(String[] args) {
+        int a = 123;
+        System.out.println((a+"").startsWith("2"));
+    }
+}
